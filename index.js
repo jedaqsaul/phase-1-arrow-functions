18e264058d170676fc917b9a6f267343f09e9a43
jedidiah <jedaqsaul@gmail.com>
2025-03-18 15:25:03 +0300

    Completed the arrow functions codegrade assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+  return 2000 / 100;
+}
+
+console.log(divide());
+
+const square = (num) => {
+  return num * num;
+};
+console.log(square(4));
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(add(6, 6));
